60871deb6a5cc7771c0e3128ff5cac30b5106d38
Michael <mdlawrence@utexas.edu>
2017-07-12 19:32:32 -0500

    problem set

--- stalk.js
+++ stalk.js
@@ -1,22 +1,36 @@
-function isEven(num){
-	if(num%2===0){
-		alert("even");
-		return true;
+function printReverse(arr){
+	var i = arr.length - 1;
+	while(i>=0){
+		console.log(arr[i]);
+		i--;
 	}
-	else{
-		alert("odd");
-		return false;
-	}
+}	
+function isUniform(arr){
+	arr.forEach(function(val){
+		if(val!==arr[0]){
+			return false;
+		}
+	});
+	return true;
 }
-function factorial(num){
-	ans = 1;
-	while(num > 1){
-		ans *= num;
-		num -= 1;
-	}
-	alert(ans);
+function sumArray(arr){
+	var sum = 0;
+	arr.forEach(function(val){
+		sum += val;
+	});
+	return sum;
 }
-isEven(5);
-isEven(8);
-factorial(4);
-factorial(3);
+function max(arr){
+	max = arr[0];
+	arr.forEach(function(val){
+		if(max<val){
+			max = val;
+		}
+	});
+	return max;
+}
+printReverse(['d','c','b','a']);
+var a = isUniform(['a','a','a']);
+var b = isUniform(['a','a','b']);
+var c = sumArray([1,2,3,4]);
+var d = max([5,4,3,7,2,1]);
