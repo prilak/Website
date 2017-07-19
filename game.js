b622d8023c0a529f1e481465673dda1a2d1983ba
Michael <mdlawrence@utexas.edu>
2017-07-19 00:13:11 -0500

    using pixelArray from p5.js

--- game.js
+++ game.js
@@ -1,14 +1,30 @@
-function mapCreate(){
-    var map = new Array(100);
-    var hMap = document.getElementById("map");
-    var hMapCtx = hMap.getContext("2d");
-    for(var i=0;i<100;i++){
-        map[i] = new Array(100);
-        for(var j=0;j<100;j++){
-            map[i][j] = 's';//sky
-            hMapCtx.rect(4*i,4*j,4,4);
-            hMapCtx.stroke();
+function setup(){
+    createCanvas(1000, 1000);
+}
+function draw(){
+    background(200);
+    loadPixels();
+    //for(var i = 0; i<500; i++){
+    //    for(var j = 0; j<width; j++){
+    //        pixels[j + width * j] = color(255,0,0);
+    //    }
+    //} 
+    var floorHeight = Math.floor(Math.random() * 20) + 800;
+    for(var j = 0; j<height; j++){
+        for(var i=0; i<width; i++){
+            index = (j + width*i)*4;
+            if(j>=floorHeight-4&&j<=floorHeight+4){
+                pixels[index+0]= 0;
+                pixels[index+1]= 0;
+                pixels[index+2]= 0;
+            }
+            else {
+                pixels[index+0]= 0;
+                pixels[index+1]= 0;
+                pixels[index+2]= 255;
+            }
+            //pixels[index+3]= 0;
         }
     }
-    alert(map[0][1]);
+    updatePixels();
 }
