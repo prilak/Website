dd815d6eab94d0954fb4675f4e6427316b104bf0
Michael <mdlawrence@utexas.edu>
2017-08-16 21:15:32 -0500

    moving game related files to Game

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
-var stuff = require('./other');
+var express = require('express');
+var mysql = require('mysql');
+//create connection
+var db = mysql.createConnection({
+	host : 'localhost',
+	user : 'michael',
+	password : 'password',
+	database : 'game_map' 
+});
+var app = express();
 
 
-var sayBye = function(){
-    console.log('bye');
-};
-var time = 0;
-console.log('hey essay');
-setTimeout(function(){
-    console.log('3sec have passed');
-}, 3000);
-var timer = setInterval(function(){
-    time += 2;
-    console.log(time+'has passed');
-    if (time > 5){
-        clearInterval(timer);
-    }
-}, 2000)
-console.log(stuff.counter(['a', 'b']));
-console.log(__dirname);
-console.log(__filename);
-//normal func
+//connect
+db.connect(function(err){
+	if (err) throw err;
+	console.log("connected");
+});
+app.get('/', function(req, resp){
+	db.query("SELECT * FROM map_features", function(error, rows, fields){
+		if(error){
+			console.log("error in query");
+		} else {
+			console.log("successful query!!");
+			console.log(rows);
+			console.log(fields);
+		}
+	});
+});
+app.listen(1337);
 
-sayBye();
-function callFunction(fun){
-    fun();
-};
-callFunction(sayBye);
-console.log(stuff.adder(1,3));
-console.log(stuff.adder(stuff.pi,3));
+//testing how to load a html
+var http = require('http');
+var fs = require('fs');
+eval(fs.readFileSync('./characters.js').toString());
+eval(fs.readFileSync('./game.js').toString());
+eval(fs.readFileSync('./map.js').toString());
+
+//var characters = require('./characters');
+const PORT=8080; 
+
+fs.readFile('./game.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
+//create db
+/*app.get('/createdb', (req) => {
+	var sql = 'CREATE DATABASE game';
+	db.query(sql, (err, result) => {
+		if(err){
+			throw err;
+		}
+		console.log(result);
+		res.send('Database created');
+
+	})
+});
+
+
+app.listen('3000', () => {
+	console.log('Server has started');
+});
+
+*/
---
+++ udemy_class/learn.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Base title</title>
+</head>
+<body>
+<?php
+    echo "Website Title";
+
+?>
+
+</body>
+</html>
